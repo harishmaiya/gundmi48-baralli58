8003f5d0bdc45e11e4b5d5a0865917e0136ba06e
Ubuntu <ubuntu@ip-172-31-18-34.us-west-2.compute.internal>
2017-03-30 08:51:21 +0000

    added deploy script and  minor updates

--- fc_smtp_settings.php
+++ fc_smtp_settings.php
@@ -1,6 +1,6 @@
 <?php 
 $config['smtp_host'] = 'ssl://smtp.gmail.com'; 
 $config['smtp_port'] = '465'; 
-$config['smtp_user'] = 'teamtweaksdevteam@gmail.com'; 
-$config['smtp_pass'] = 'teamtweaksdevteam!'; 
+$config['smtp_user'] = 'baysideyardparties@gmail.com'; 
+$config['smtp_pass'] = 'kidsyard#45'; 
  ?>
